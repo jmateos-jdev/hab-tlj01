d101561a9ba45756282e674ce91952e62ee43039
unknown <joaquin.mateos@jdev.com.ar>
2025-05-14 12:30:45 -0300

    'update'

--- node/index.js
+++ node/index.js
@@ -5,17 +5,14 @@ const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.setHeader("Content-Type", "application/json");
+  res.setHeader("Content-Type", "application/json");
 
-    const response = routeador(req.method, req.url);
+  const response = routeador(req.method, req.url);
 
-    res.statusCode = response?.status || 404;
-    res.end(JSON.stringify(response));
-
+  res.statusCode = response?.status || 404;
+  res.end(JSON.stringify(response));
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-
